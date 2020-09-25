2f6a428eb36626b4f084dca9592995895fd8a353
stepin104930 <69069498+stepin104930@users.noreply.github.com>
2020-09-25 18:26:37 +0530

    Update election_operations.h

--- inc/election_operations.h
+++ inc/election_operations.h
@@ -1,6 +1,5 @@
 /** 
-* @file calculator_operations.h
-* Calculator application with 4 mathematical operations
+* @file election_operations.h
 *
 */
 #ifndef __ELECTION_OPERATIONS_H__
@@ -12,7 +11,6 @@
 #include <stdio_ext.h>
 
 /**
-*  adds the operand1 and operand2 and returns the result
 * @param[in] operand1 
 * @param[in] operand2 
 * @return Result of operand1 + operand2
@@ -20,7 +18,6 @@
 int forvote(int operand1, int operand2);
 
 /**
-*  subtracts the operand1, operand2 and returns the result
 * @param[in] operand1 
 * @param[in] operand2 
 * @return Result of operand1 - operand2
@@ -28,7 +25,6 @@ int forvote(int operand1, int operand2);
 int newcandidate(int operand1, int operand2);
 
 /**
-*  multiply the operand1, operand2 and returns the result
 * @param[in] operand1 
 * @param[in] operand2 
 * @return Result of operand1 * operand2
@@ -36,22 +32,16 @@ int newcandidate(int operand1, int operand2);
 int listofvoters(int operand1, int operand2);
 
 
-/**
-* divides the operand1 by operand2 and returns the result 
+/** 
 * @param[in] operand1 
 * @param[in] operand2 
-* @return integer value of the operand1 / operand2
-* @note returns 0 for divide by 0 error
 */
 int livecount(int operand1, int operand2);
 
-/**
-* divides the operand1 by operand2 and returns the result 
+/** 
 * @param[in] operand1 
 * @param[in] operand2 
-* @return integer value of the operand1 / operand2
-* @note returns 0 for divide by 0 error
 */
 int listofcandidates(int operand1, int operand2);
 
-#endif  /* #define __CALCULATOR_OPERATIONS_H__ */
+#endif  /* #define __ELECTION_OPERATIONS_H__ */
